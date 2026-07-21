f9c214a61faa1946ae092f6d845b3e9df8d76165
iajibose <ajiboseibrahim12@gmail.com>
2026-07-21 15:41:37 +0100

    Stage 5: Swagger UI

--- main.py
+++ main.py
@@ -37,19 +37,23 @@ tasks: list[Task] = [
 
 @app.get("/")
 async def root():
+    """Describe the API"""
     return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
 
 @app.get("/health")
 async def check_health():
+    """Checks the status of the API"""
     return {"status": "ok"}
 
 
 @app.get("/tasks")
 async def get_all_tasks():
+    """Retrived all stored tasks"""
     return tasks
 
 @app.get("/tasks/{id}")
 async def get_task(id: int):
+    """Get task with id from the stored tasks or 404 if not found"""
     for task in tasks:
         if task["id"] == id:
             return task
@@ -58,6 +62,9 @@ async def get_task(id: int):
 
 @app.post("/tasks", status_code=201)
 async def create_task(task: TaskGet):
+    """Create a new task
+        Return 400 if title is absent or empty
+    """
     if not task.title:
         return JSONResponse(status_code=400, content={"error": "title is empty"})
 
@@ -74,6 +81,9 @@ async def create_task(task: TaskGet):
 
 @app.put("/tasks/{id}", status_code=200)
 async def update_task(id: int, data: TaskUpdate):
+    """Update task with the id
+    Return 404 if no task with that id found
+    """
     task = next((task for task in tasks if task["id"] == id), None)
     if not task:
         return JSONResponse(status_code=404, content={"error": f"Task with id {id} not found"})
@@ -88,6 +98,10 @@ async def update_task(id: int, data: TaskUpdate):
     
 @app.delete("/tasks/{id}", status_code=204)
 async def remove_task(id: int):
+    """Remove a task from the stored tasks
+
+    Return 404 if no task found
+    """
     task = next((task for task in tasks if task["id"] == id), None)
     if not task:
         return JSONResponse(status_code=404, content={"error": f"Task with id {id} not found"})
